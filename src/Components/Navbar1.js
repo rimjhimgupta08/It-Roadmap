fe89d9e8c9a2ecb6d0306f12be2924aaf81dc67d
Rimjhim Gupta <rimjhimgupta786@gmail.com>
2024-04-18 11:40:47 +0530

    change name

--- src/Components/Navbar1.js
+++ src/Components/Navbar1.js
@@ -9,7 +9,7 @@ import './Mediaquery.css';
 
 
 const Navbar1 = () => {
-  const userName =  JSON.parse(localStorage.getItem('user'));
+//   const userName =  JSON.parse(localStorage.getItem('user'));
 
   return (
       
@@ -18,7 +18,7 @@ const Navbar1 = () => {
           <Navbar expand="lg" className="bg-body-tertiary navbar">
             <Container>
               <Navbar.Brand as = {Link}  to="/"><img src="./image/it.jpeg" alt="" width='80px' /> Tech Skills</Navbar.Brand>
-   <p className='text-center'>Welcome - {userName.name}</p>
+   {/* <p className='text-center'>Welcome - {userName.name}</p> */}
 
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav" className='collapse'>
