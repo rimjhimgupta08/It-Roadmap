a23379222b2cc0728181b869d438ccf3a886f2b8
Rimjhim Gupta <rimjhimgupta786@gmail.com>
2024-04-18 19:41:37 +0530

    change footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -50,11 +50,16 @@ const Footer = () => {
                               <div className="col-md-4 col-sm-6">
                               <h3>Get in touch  </h3>
                               <li>rimjhimgupta786@gmail.com</li>
-                              <li>facebook</li>
-                              <li>Github</li>
-                              <li>Linkedin</li>
-                              <li>Instagram</li>
+                              
 
+                              <Nav className="mt-0 d-block footer-nav ">
+                  <Nav.Link as = {Link} to="https://www.facebook.com/profile.php?id=100066771017472" className='active' >Facebook</Nav.Link>
+                  <Nav.Link as = {Link} to="https://github.com/rimjhimgupta08">Github</Nav.Link>
+                  <Nav.Link as = {Link} to="https://www.linkedin.com/in/rimjhim-gupta-95a872229/">LinkedIn</Nav.Link>
+                  <Nav.Link as = {Link} to="https://www.instagram.com/rimjhim__08/" >Instagram</Nav.Link>
+
+                  
+                </Nav>
                                     
                               </div>
                         </div>
